642abf5178f51a1c49250448c75e1326d850d000
Tarika Birch <tarikabirch@gmail.com>
2025-09-18 10:36:03 -0400

    Created nav bar and added page routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Transactions } from './transactions/transactions';
+import { Analytics } from './analytics/analytics';
+import { Profile } from './profile/profile';
+
+export const routes: Routes = [
+	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+	{ path: 'dashboard', component: Dashboard },
+	{ path: 'transactions', component: Transactions },
+	{ path: 'analytics', component: Analytics },
+	{ path: 'profile', component: Profile },
+	{ path: '**', redirectTo: 'dashboard' }
+];
