d42b3470e34ec6957d4952c0e2082052be4a68e9
Tarika Birch <tarikabirch@gmail.com>
2025-09-22 10:38:16 -0400

    Added dynamic transaction info to transactions page

--- frontend/src/app/pages/transactions/transactions.ts
+++ frontend/src/app/pages/transactions/transactions.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { ActivityCard } from '../../shared/components/activity-card/activity-card';
+import { Transaction, UserTransactions } from '../../services/user-transactions/user-transactions';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-transactions',
-  imports: [ActivityCard],
+  imports: [ActivityCard, CommonModule],
   templateUrl: './transactions.html',
-  styleUrl: './transactions.scss'
+  styleUrl: './transactions.scss',
 })
 export class Transactions {
+  transactions: Transaction[] = [];
 
+  constructor(private userTransactionService: UserTransactions) {}
+
+  ngOnInit() {
+    try {
+      this.userTransactionService.getTransactions().subscribe((data) => {
+        this.transactions = data;
+      });
+    } catch (error) {
+      console.error('Error fetching transactions:', error);
+    }
+  }
 }
